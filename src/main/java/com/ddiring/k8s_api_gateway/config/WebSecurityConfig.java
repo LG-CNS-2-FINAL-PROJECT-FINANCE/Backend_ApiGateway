6fee68363dabdf2c72f09c731bc4003e9f4cdc90
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2025-08-19 16:04:46 +0900

    테스트용 푸쉬

--- src/main/java/com/ddiring/k8s_api_gateway/config/WebSecurityConfig.java
+++ src/main/java/com/ddiring/k8s_api_gateway/config/WebSecurityConfig.java
@@ -71,7 +71,7 @@ public class WebSecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-//        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
