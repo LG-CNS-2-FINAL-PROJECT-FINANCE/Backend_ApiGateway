f7e573d43449f922e030d032398a6e216dfa14f0
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2025-08-21 16:42:55 +0900

    테스트용 푸쉬

--- src/main/java/com/ddiring/k8s_api_gateway/security/jwt/props/JwtConfigProperties.java
+++ src/main/java/com/ddiring/k8s_api_gateway/security/jwt/props/JwtConfigProperties.java
@@ -11,6 +11,6 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 public class JwtConfigProperties {
-    private String header = "Authorization";
+    private String header;
     private String secretKey;
 }
